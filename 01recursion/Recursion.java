e684aa13f5879a89b0c2689356b59d41ad0eae84
Kathy Wang <k.wang6898@gmail.com>
2015-02-05 23:06:24 -0500

    updated with other two functions, things work! what!!

--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -1,9 +1,9 @@
 public class Recursion{
-
+    
     public String name(){ 
 	return "Wang, Kathy"; 
     }
-
+    
     public int fact(int n){
 	if(n<0){
 	    throw new IllegalArgumentException("Can't evaluate a negative number.");
@@ -18,17 +18,49 @@ public class Recursion{
     }
     
     public int fib(int n){
-	return 0;
+	if(n<0){
+	    throw new IllegalArgumentException("Can't evaluate a negative number.");
+	}
+	if(n==0) return 0; 
+	if(n==1 || n==2){
+	    return 1;
+	}
+	return fib(n-1)+fib(n-2); 
     }
     
     public double sqrt(double n){
-	return 0.0;  
-  }
+	if(n<0){
+	    throw new IllegalArgumentException("Can't evaluate a negative number.");
+	}
+	if(n==0){
+	    return 0;
+	}
+	return sqrtHelp(n,1.0);
+    }
+
+    public double sqrtHelp(double n, double guess){
+	double newGuess = (n/guess + guess)/2;
+	if(n==0){
+	    return 0;
+	}
+	if(newGuess==guess){
+	    return guess;
+	}
+	return sqrtHelp(n,newGuess); 
+    }
     
     public static void main(String[]args){
 	Recursion a = new Recursion(); 
-	System.out.println(a.fact(0));
-	System.out.println(a.fact(5));
-	System.out.println(a.fact(-10)); 
+	//	System.out.println(a.fact(0));
+	//	System.out.println(a.fact(5));
+	//	System.out.println(a.fact(-10)); 
+      	System.out.println(a.fib(5));
+	//	System.out.println(a.fib(-1));
+	System.out.println(a.fib(7));
+	System.out.println(a.fib(0));
+	System.out.println(a.sqrt(100)); 
+	System.out.println(a.sqrt(144));
+	System.out.println(a.sqrt(50));
+	System.out.println(a.sqrt(-100));  
     }
 }
